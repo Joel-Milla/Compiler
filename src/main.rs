c12e92fe50539921b2bd2f57de8e64e837127079
Joel-Milla <jalejandro.milla@outlook.com>
2026-06-04 00:17:08 -0600

    Remove unused code from main.rs.

--- src/main.rs
+++ src/main.rs
@@ -7,52 +7,6 @@ mod quadruples;
 use compiler::Compiler;
 
 fn main() {
-    let advanced_programa = "
-        programa miPrograma;
-        vars
-            x, y : entero;
-            z : flotante;
-        
-        flotante function1(id1:entero, sincero:flotante, hola:entero){
-            vars
-                var1, x, y : flotante;
-            {
-                asignacion = 2 + 4;
-                mientras(true) haz {escribe('hola');};
-            }
-        };
-        inicio
-            {
-                x = 5;
-                y = x + 3;
-                si (x > y) {
-                    x = x + 1;
-                } sino {
-                    y = y + 1;
-                };
-                mientras (x < 10) haz {
-                    x = x + 1;
-                };
-                escribe ('resultado:', x);
-            }
-        fin
-    ";
 
-    let programa_basico = "
-        programa miPrograma;
-        vars
-            x : entero;
-            x : flotante;
-        inicio
-            {
-                x = 5;
-            }
-        fin
-    ";
-    let mut compiler = Compiler::new();
-    match compiler.compile_program(programa_basico) {
-        Ok(_)  => println!("Compiled successfully"),
-        Err(e) => println!("Error: {:?}", e),
-    };
 }
 
